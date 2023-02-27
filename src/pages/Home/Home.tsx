3dfd303d27acaa8b2f773b19708a906b7c8427fd
KyryloP <peresada1997@gmail.com>
2023-02-27 12:11:17 +0200

    router overlogic deleted

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,4 @@
-import { Navigate } from "react-router-dom";
-import { loginStatusSelector } from "../../modules/LoginStatus/features/selector";
-import { useAppSelector } from "../../shared/hooks/useAppSelector";
-
-
 export const Home = () => {
-  const {isAuthorized} = useAppSelector(loginStatusSelector);
-  if(!isAuthorized){
-    return <Navigate to="/login" replace />;
-  }
-
   return(
     <div>Home Page</div>
   );
--- src/pages/Logs/Logout.tsx
+++ src/pages/Logs/Logout.tsx
@@ -1,23 +1,13 @@
-import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 import { authorize } from "../../modules/LoginStatus/features/reducer";
-import { loginStatusSelector } from "../../modules/LoginStatus/features/selector";
 import { useAppDispatch } from "../../shared/hooks/useAppDispatch";
-import { useAppSelector } from "../../shared/hooks/useAppSelector";
 import { StyledLogButton, StyledLogoutBlock } from "./styles";
 
 export const Logout = () => {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
-  const {isAuthorized} = useAppSelector(loginStatusSelector);
   const {t} = useTranslation();
-
-  useEffect(() => {
-    if(!isAuthorized){
-      navigate("/login", {replace: true});
-    }
-  });
 
   const logout = () => {
     dispatch(authorize());
