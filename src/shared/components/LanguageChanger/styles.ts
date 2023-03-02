0ba6e52e6ee56f71edb911819b0de25c82e89c3e
KyryloP <peresada1997@gmail.com>
2023-03-02 11:58:58 +0200

    language changer styles

--- src/shared/components/LanguageChanger/styles.ts
+++ src/shared/components/LanguageChanger/styles.ts
@@ -1,9 +1,14 @@
 import styled from "styled-components";
+import { ScreenSizes } from "../../helpers/themes";
 
 export const StyledLanguageChangerWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  font-size: 16px;
+  @media screen and (min-width: ${ScreenSizes.LAPTOP_S}px) {
+    font-size: 20px;
+  };
 `;
 
 export const StyledLanguageChangerSelect = styled.select`
