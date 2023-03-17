4ad72d63edff80aecfaf9c1cc8b484b4216d4085
KyryloP <peresada1997@gmail.com>
2023-03-17 11:11:14 +0200

    remove document from react

--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -27,9 +27,9 @@ export const Users = () => {
   }, [fetching, isLoading, users, dispatch, currentPage]);
 
   useEffect(() => {
-    document.addEventListener("scroll", scrollHandler);
+    window.addEventListener("scroll", scrollHandler);
     return (() => {
-      document.removeEventListener("scroll", scrollHandler);
+      window.removeEventListener("scroll", scrollHandler);
     });
   });
 
