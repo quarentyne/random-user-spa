afbdf7acade8bc6398c4612e7cab002603c46812
KyryloP <peresada1997@gmail.com>
2023-03-21 15:57:31 +0200

    login rebuild

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,30 +1,25 @@
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 import { authorize } from "../../modules/Login/features/loginStatusSlice";
-import { loginStatusSelector } from "../../modules/Login/features/selector";
 import { ROUTES_PATHS } from "../../shared/constants/routes";
 import { useAppDispatch } from "../../shared/hooks/useAppDispatch";
-import { useAppSelector } from "../../shared/hooks/useAppSelector";
 import { StyledLogButton, StyledLogBlock } from "./styles";
 
 export const Login = () => {
   const navigate = useNavigate();
   const { t } = useTranslation();
   const dispatch = useAppDispatch();
-  const { isAuthorized } = useAppSelector(loginStatusSelector);
 
   const handleAuthorize = () => {
     dispatch(authorize());
-    localStorage.setItem("login", isAuthorized ? "" : "true");
-    if (!isAuthorized) {
-      navigate(ROUTES_PATHS.HOME, { replace: true });
-    }
+    localStorage.setItem("login", "true");
+    navigate(ROUTES_PATHS.HOME, { replace: true });
   };
 
   return (
     <StyledLogBlock>
       <StyledLogButton type="submit" onClick={handleAuthorize}>
-        {t(`sideMenu.${isAuthorized ? "logout" : "login"}`)}
+        {t(`sideMenu.login`)}
       </StyledLogButton>
     </StyledLogBlock>
   );
--- src/shared/components/NavigationList/NavigationList.tsx
+++ src/shared/components/NavigationList/NavigationList.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import { useTranslation } from "react-i18next";
 import {
   StyledLinkImage,
@@ -13,10 +14,17 @@ import logout from "../../../assets/svg/logout.svg";
 import { ROUTES_PATHS } from "../../constants/routes";
 import { useAppSelector } from "../../hooks/useAppSelector";
 import { loginStatusSelector } from "../../../modules/Login/features/selector";
+import { authorize } from "../../../modules/Login/features/loginStatusSlice";
 
 export const NavigationList = () => {
   const { t } = useTranslation();
   const { isAuthorized } = useAppSelector(loginStatusSelector);
+  const dispatch = useDispatch();
+
+  const handleAuthorize = () => {
+    dispatch(authorize());
+    localStorage.setItem("login", "");
+  };
 
   return (
     <StyledNavigationList isAuthorized={isAuthorized}>
@@ -38,10 +46,12 @@ export const NavigationList = () => {
           <span>{t(`sideMenu.userInfo`)}</span>
         </StyledNavigationLink>
       </StyledNavigationItem>
-      <StyledNavigationItem>
+      <StyledNavigationItem onClick={isAuthorized ? handleAuthorize : () => {}}>
         <StyledNavigationLink to={ROUTES_PATHS.LOGIN}>
           <StyledLinkImage src={logout} alt="logout" />
-          <span>{t(`sideMenu.logout`)}</span>
+          <span>
+            {t(`${isAuthorized ? "sideMenu.logout" : "sideMenu.login"}`)}
+          </span>
         </StyledNavigationLink>
       </StyledNavigationItem>
     </StyledNavigationList>
